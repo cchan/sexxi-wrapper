bbf54d2b0f2ea40515b3554ea6682007d4520bbc
Clive Chan <cc@clive.io>
2016-08-10 05:53:51 -0400

    it works -- apparently it's helmet that's failing (don't need it anyway); various fixes

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ var express = require('express');
 var app = express();
 var cp = require('child_process');
 
-app.use(require('helmet'));
+//app.use(require('helmet'));
 
 var website;
 
 function update(){
+  console.log("updating");
+
   if(typeof website != "undefined"){
     website.kill();
     website.disconnect();
@@ -25,15 +27,22 @@ function update(){
   });
 
   website.on('exit', function (code) {
-    console.log('child process exited with code ' + code.toString());
-    website.disconnect()
-    website = undefined;
+    if(code === null)
+      console.log('child process forced to exit');
+    else{
+      console.log('child process exited with code ' + code.toString());
+      website.disconnect()
+      website = undefined;
+    }
   });
 }
 
-app.post('/update', update);
+app.post('/update', function(req, res){
+  update();
+  res.send('done');
+});
 
-app.listen('61792', 'localhost', function(){
+app.listen(61792, 'localhost', function(){
   console.log('[wrapper] listening 61792');
   update();
 });
